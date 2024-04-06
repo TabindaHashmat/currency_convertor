50d0af4de9101bcda92b7b60b5fab98ca5e048af
Tabinda Hashmat <159946123+TabindaHashmat@users.noreply.github.com>
2024-04-06 22:30:45 +0500

    Add files via upload
    here is currency convertor for all type of currency

---
+++ index.ts
@@ -0,0 +1,40 @@
+import inquirer from "inquirer";
+
+const currency : any={
+   USD: 1,
+   EUR: 0.91,
+   GBP: 0.76,
+   INR: 74.57,
+  PKR: 280
+};
+
+let user_answer = await  inquirer.prompt([  
+    {
+        name: 'from',
+    message: "Enter from currency",
+     type: 'list',
+     choices: ['USD','EUR','GBP','INR','PKR']
+},
+{name: 'to',
+message: "Enter from currency",
+ type: 'list',
+ choices: ['USD','EUR','GBP','INR','PKR']
+},
+{
+name: 'amount',
+type: 'number',
+message: "enter your Amount",
+}
+]
+)
+//dynamic approach
+
+let fromAmount = currency[user_answer.from]//exchange rate
+let toAmount = currency[user_answer.to]//exchange rate
+let amount = user_answer.amount
+let baseAmount = amount / fromAmount //usd base currency //4
+let convertedAmount = baseAmount * toAmount
+console.log(convertedAmount);
+// console.log(fromAmount);
+// console.log(toAmount);
+// console.log(amount);
